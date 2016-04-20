d4ec57565c3121e1d39ebd32a4fe30d2f116b4b5
Evandro Junior <evandrojr@gmail.com>
2016-04-20 17:21:47 -0300

    Improved view index

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -9,4 +9,13 @@ class Meeting < ActiveRecord::Base
     "#{d.in_hours.to_i.to_s.rjust(2, '0')}:#{d.in_minutes.to_i.to_s.rjust(2, '0')}:#{d.in_seconds.to_i.to_s.rjust(2, '0')}"
   end
 
+  def participants_total_seconds
+    TimeDifference.between(created_at, finish).in_seconds * participants
+  end
+
+  def total_time_for_humans
+    participants_total_seconds
+
+  end
+
 end
